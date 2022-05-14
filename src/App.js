ad2fb2865f8dbaff87e9471f815e0976f7da2e91
Eric Fithian <ef2413@columbia.edu>
2022-05-14 11:03:46 -0400

    Updated 2nd time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import StarshipPage from './components/StarshipPage';
 import StarshipsList from './components/StarshipsList';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
+  const [ships, setShips] = useState(null)
+
+  function getShips () {
+    fetch('https://swapi.dev/api/starships')
+    .then((res) => res.json())
+    .then((res) => setShips(res))
+  }
+
+  useEffect(() => {
+    getShips()
+  }, [])
+  // console.log(ships)
+  if(!ships) {
+    return( 
+      <>
+        <h2>Loading</h2>
+      </>
+    )
+  }
   return (
     <Routes>
       <Route path='/' element={<StarshipsList />} />
